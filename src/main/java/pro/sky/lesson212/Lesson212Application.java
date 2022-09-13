78e5554ca5734d7c29e9faf08cd8d29bfdbfbead
pavel_gorpynyak <pavel_gorpynyak@bk.ru>
2022-09-13 08:23:53 +0300

    Initial commit

---
+++ src/main/java/pro/sky/lesson212/Lesson212Application.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson212Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson212Application.class, args);
+	}
+
+}
